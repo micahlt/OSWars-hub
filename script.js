57327942112f65e2e69b1da7c13789ecaa56a58e
Micah Lindley <micahincorporated@gmail.com>
2020-01-24 12:01:43 -0600

    Initialize webpage from Repl.it

---
+++ script.js
@@ -0,0 +1,9 @@
+function showDescription() {
+  var d = document.getElementById("OSWARS");
+  d.className += " addedclass";
+}
+
+function closeDescription() {
+  var d = document.getElementById("OSWARS");
+  d.className = "description-os";
+}
